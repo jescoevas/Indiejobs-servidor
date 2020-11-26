29c64434359dbb45c3aa89df7eecb3aedb359675
Jesus Coello <jescoevas@hotmail.com>
2020-11-26 19:16:04 +0100

    Base de datos en la nube

--- src/config/env.js
+++ src/config/env.js
@@ -3,9 +3,9 @@ const puerto = process.env.PORT || 3000
 
 //Base de datos
 const desarrollo = 'mongodb://localhost:27017/indiejobs'
-// const produccion = 'mongodb+srv://jescoevas:1h5bjrwnsZYIKNFF@cluster0.hru6d.mongodb.net/indiejobs'
+const produccion = 'mongodb+srv://jescoevas:1h5bjrwnsZYIKNFF@cluster0.hru6d.mongodb.net/indiejobs'
 
-const baseDeDatos = /* process.env.PORT ? prod : */ desarrollo
+const baseDeDatos = process.env.PORT ? produccion :  desarrollo
 
 module.exports = {
     puerto, baseDeDatos
