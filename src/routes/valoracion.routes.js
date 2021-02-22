496849f0d2af1e0b58692ea8bd73ae5fea39fe20
Jesus Coello <jescoevas@hotmail.com>
2021-02-22 20:26:47 +0100

    Sprint 3

--- src/routes/valoracion.routes.js
+++ src/routes/valoracion.routes.js
@@ -11,8 +11,6 @@ router.post('/trabajo/valoracion', verificarToken, async (req, resp) => {
     const autor = await Usuario.findById(req.usuario._id)
     const trabajo = await Trabajo.findById(trabajoId)
     const valoracion = await Valoracion.find({autor, trabajo})
-    console.log(`${autor.nombre} ha hecho ${valoracion.length} valoraciones a ${trabajo.cuerpo}`)
-    console.log(valoracion)
     return resp.json({encontrado:valoracion.length > 0})
 })
 
@@ -24,7 +22,6 @@ router.post('/trabajo/valorar/sumar',verificarToken, async (req, resp) => {
     if(valoracion.length > 0){
         return resp.json({msg:'Ya existe la valoracion'})
     }else{
-        console.log(`El autor ${autor.nombre} suma valoracion a ${trabajo.cuerpo}`)
         await Valoracion.create({autor, trabajo})
         const newEstrellas = trabajo.estrellas + 1
         const newTrabajo = await Trabajo.findByIdAndUpdate(trabajo._id, {$set:{estrellas:newEstrellas}}, {new:true})
@@ -36,14 +33,10 @@ router.post('/trabajo/valorar/restar',verificarToken, async (req, resp) => {
     const {trabajoId} = req.body
     const autor = await Usuario.findById(req.usuario._id)
     const trabajo = await Trabajo.findById(trabajoId)
-    console.log(`El trabajo es: ${trabajo.cuerpo}`)
-    console.log(`El autor es: ${autor.nombre}`)
     const valoracion = await Valoracion.find({autor, trabajo})
-    console.log(`La valoracion es: ${valoracion}, con ID: ${valoracion[0]._id}`)
     if(valoracion.length == 0){
         return resp.json({msg:'No existe la valoracion'})
     }else{
-        console.log(`El autor ${autor.nombre} resta valoracion a ${trabajo.cuerpo}`)
         await Valoracion.findOneAndRemove({_id:valoracion[0]._id})
         const newEstrellas = trabajo.estrellas - 1
         const newTrabajo = await Trabajo.findByIdAndUpdate(trabajo._id, {$set:{estrellas:newEstrellas}}, {new:true})
