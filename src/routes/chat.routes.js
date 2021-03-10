321da7834e43390454a932561a43508bc25991ce
Jesus Coello <jescoevas@hotmail.com>
2021-03-10 21:58:15 +0100

    Sprint4

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -1,4 +1,4 @@
-const { Router } =  require('express')
+const { Router } = require('express')
 const Usuario = require('../models/usuario.model')
 const Chat = require('../models/chat.model')
 const Mensaje = require('../models/mensaje.model')
@@ -6,44 +6,42 @@ const verificarToken = require('../tools/verificarToken')
 
 const router = Router()
 
-router.get('/chat/:chatId', async (req, resp) => {
+router.post('/chat/mios', verificarToken, async(req, resp) => {
+    const usuario = req.usuario
+    const chats = await Chat.find({ $or: [{ 'usuario1': usuario }, { 'usuario2': usuario }] })
+    resp.json({ chats })
+})
+
+router.get('/chat/:chatId', async(req, resp) => {
     const { chatId } = req.params
     const chat = await Chat.findById(chatId)
-    console.log(chatId, chat)
-    const mensajes = await Mensaje.find({chat})
-    console.log(mensajes)
-    resp.json({mensajes})
+    const mensajes = await Mensaje.find({ chat })
+    resp.json({ mensajes })
 })
 
-router.post('/chat/mios', verificarToken, async (req, resp) => {
-    const usuario = req.usuario
-    const chats = await Chat.find({$or:[  {'usuario1':usuario}, {'usuario2':usuario} ]})
-    resp.json({chats})
-})
-
-router.post('/chat/create',verificarToken, async (req, resp) => {
-    const usuario1 = req.usuario
-    const { usuario2ID} = req.body
-    const usuario2 = await Usuario.findById(usuario2ID)
-    const chat = await Chat.create({usuario1, usuario2})
-    resp.json({chat})
-})
-
-router.post('/mensaje/create',verificarToken, async (req, resp) => {
-    const autor = req.usuario
-    const { chatId, cuerpo} = req.body
-    const chat = await Chat.findById(chatId)
-    const mensaje = await Mensaje.create({chat, autor, cuerpo})
-    resp.json({mensaje})
-})
-
-router.post('/chat/existe', verificarToken, async (req, resp) => {
+router.post('/chat/create', verificarToken, async(req, resp) => {
     const usuario1 = req.usuario
     const { usuario2ID } = req.body
     const usuario2 = await Usuario.findById(usuario2ID)
-    const chats1 = await Chat.find({usuario1, usuario2})
-    const chats2 = await Chat.find({usuario1:usuario2, usuario2:usuario1})
-    resp.json({encontrado:chats1.length > 0 || chats2.length > 0})
+    const chat = await Chat.create({ usuario1, usuario2 })
+    resp.json({ chat })
+})
+
+router.post('/mensaje/create', verificarToken, async(req, resp) => {
+    const autor = req.usuario
+    const { chatId, cuerpo } = req.body
+    const chat = await Chat.findById(chatId)
+    const mensaje = await Mensaje.create({ chat, autor, cuerpo })
+    resp.json({ mensaje })
+})
+
+router.post('/chat/existe', verificarToken, async(req, resp) => {
+    const usuario1 = req.usuario
+    const { usuario2ID } = req.body
+    const usuario2 = await Usuario.findById(usuario2ID)
+    const chats1 = await Chat.find({ usuario1, usuario2 })
+    const chats2 = await Chat.find({ usuario1: usuario2, usuario2: usuario1 })
+    resp.json({ encontrado: chats1.length > 0 || chats2.length > 0 })
 })
 
 module.exports = router
