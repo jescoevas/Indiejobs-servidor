7a9cf6b61b7396a66d7285f3db403468658ccd9a
Jesus Coello <jescoevas@hotmail.com>
2021-03-29 20:20:39 +0200

    Nueva base de datos

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,4 +1,4 @@
-const { Router } =  require('express')
+const { Router } = require('express')
 const Usuario = require('../models/usuario.model')
 const Token = require('../tools/token')
 const verificarToken = require('../tools/verificarToken')
@@ -8,152 +8,153 @@ const Trabajo = require('../models/trabajo.model')
 const router = Router()
 const usuarioFotos = new UsuarioFotos()
 
-router.get('/usuario/:usuarioId', async (req, res) => {
-    const {usuarioId} = req.params
+router.get('/usuario/:usuarioId', async(req, res) => {
+    const { usuarioId } = req.params
     const usuario = await Usuario.findById(usuarioId)
-    return res.json({usuario})
+    return res.json({ usuario })
 })
 
-router.post('/registro', async (req, res) => {
+router.post('/registro', async(req, res) => {
 
     const { email, telefono } = req.body
-    const emailEncontrado = await Usuario.find({email})
-    const telefonoEncontrado = await Usuario.find({telefono})
+    const emailEncontrado = await Usuario.find({ email })
+    const telefonoEncontrado = await Usuario.find({ telefono })
 
-    if(emailEncontrado.length > 0){
+    if (emailEncontrado.length > 0) {
         return res.json({
-            msg:'Email existe',
-            usuarioDB:{}
+            msg: 'Email existe',
+            usuarioDB: {}
         })
-    }else if(telefonoEncontrado.length > 0){
+    } else if (telefonoEncontrado.length > 0) {
         return res.json({
-            msg:'Telefono existe',
-            usuarioDB:{}
+            msg: 'Telefono existe',
+            usuarioDB: {}
         })
-    }else{
-        const usuarioDB = await Usuario.create({...req.body})
+    } else {
+        const usuarioDB = await Usuario.create({...req.body })
         res.json({
-            msg:'Registro realizado con exito',
+            msg: 'Registro realizado con exito',
             usuarioDB,
-            token:Token.getJwtToken(usuarioDB)
+            token: Token.getJwtToken(usuarioDB)
         })
     }
 
 })
 
-router.put('/editar', async (req, res) => {
+router.put('/editar', async(req, res) => {
 
     const { email, telefono, id, emailUsuario, telefonoUsuario } = req.body
-    const emailEncontrado = await Usuario.find({email})
-    const telefonoEncontrado = await Usuario.find({telefono})
+    const emailEncontrado = await Usuario.find({ email })
+    const telefonoEncontrado = await Usuario.find({ telefono })
 
-    if(emailEncontrado.length > 0 && email !== emailUsuario){
+    if (emailEncontrado.length > 0 && email !== emailUsuario) {
         return res.json({
-            msg:'Email existe',
+            msg: 'Email existe',
         })
-    }else if(telefonoEncontrado.length > 0 && telefono !== telefonoUsuario){
+    } else if (telefonoEncontrado.length > 0 && telefono !== telefonoUsuario) {
         return res.json({
-            msg:'Telefono existe',
+            msg: 'Telefono existe',
         })
-    }else{
+    } else {
         delete req.body.id
         delete req.body.emailUsuario
         delete req.body.telefonoUsuario
-        await Usuario.findByIdAndUpdate(id, req.body, {new:true})
+        await Usuario.findByIdAndUpdate(id, req.body, { new: true })
         res.json({
-            msg:'Edicion realizada con exito',
+            msg: 'Edicion realizada con exito',
         })
     }
 
 })
 
-router.post('/login', async (req, res) => {
-    const {email, password} = req.body
-    const usuario = await Usuario.findOne({email})
-    if(!usuario) 
-        return res.json({msg:"No se ha encontrado el usuario"})
+router.post('/login', async(req, res) => {
+    const { email, password } = req.body
+    const usuario = await Usuario.findOne({ email })
+    if (!usuario)
+        return res.json({ msg: "No se ha encontrado el usuario" })
     const coincide = await usuario.compararPassword(password)
-    if(!coincide) 
-        return res.json({msg:"Password incorrecta"})
+    if (!coincide)
+        return res.json({ msg: "Password incorrecta" })
     res.json({
-        msg:"Login realizado con exito",
-        usuarioId:usuario._id,
-        foto:usuario.foto,
-        token:Token.getJwtToken(usuario)
+        msg: "Login realizado con exito",
+        usuarioId: usuario._id,
+        foto: usuario.foto,
+        token: Token.getJwtToken(usuario)
     })
 })
 
 router.get('/:usuarioId/foto', (req, res) => {
-    const {usuarioId} = req.params
+    const { usuarioId } = req.params
     const path = usuarioFotos.getCarpetaFoto(usuarioId)
     const foto = usuarioFotos.getFoto(usuarioId)
     const pathCompleto = `${path}/${foto}`
     res.sendFile(pathCompleto)
 })
 
-router.post('/asignarFoto', verificarToken, async (req, res) => {
-    if(!req.files) 
-        return res.json({msg:"No se han enviado archivos"})
-    const {foto} = req.files
-    if(!foto.mimetype.includes('image')) 
-        return res.json({msg:"No se ha subido ninguna foto"})
+router.post('/asignarFoto', verificarToken, async(req, res) => {
+    if (!req.files)
+        return res.json({ msg: "No se han enviado archivos" })
+    const { foto } = req.files
+    if (!foto.mimetype.includes('image'))
+        return res.json({ msg: "No se ha subido ninguna foto" })
     let usuario = req.usuario
     await usuarioFotos.asignarFoto(foto, usuario._id)
     const fotoUsuario = usuarioFotos.getFoto(usuario._id)
     usuario.foto = fotoUsuario
-    await Usuario.findByIdAndUpdate(usuario._id, usuario, {new:true})
+    await Usuario.findByIdAndUpdate(usuario._id, usuario, { new: true })
     res.json({
-        msg:"Foto asignada"
+        msg: "Foto asignada",
+        foto: fotoUsuario
     })
 })
 
-router.post('/checkEmail', async (req, resp) => {
+router.post('/checkEmail', async(req, resp) => {
     const { email } = req.body
     const encontrado = await Usuario.find({ email })
-    return resp.json({num: encontrado.length})
+    return resp.json({ num: encontrado.length })
 })
 
-router.post('/checkTelefono', async (req, resp) => {
+router.post('/checkTelefono', async(req, resp) => {
     const { telefono } = req.body
     const encontrado = await Usuario.find({ telefono })
-    return resp.json({num: encontrado.length})
+    return resp.json({ num: encontrado.length })
 })
 
-router.get('/trabajadores', async (req,resp) => {
-    const trabajadores = await Usuario.find({trabajador:true})
-    return resp.json({trabajadores})
+router.get('/trabajadores', async(req, resp) => {
+    const trabajadores = await Usuario.find({ trabajador: true })
+    return resp.json({ trabajadores })
 })
 
-router.get('/trabajadores/cercanos',verificarToken , async (req,resp) => {
+router.get('/trabajadores/cercanos', verificarToken, async(req, resp) => {
     const usuario = req.usuario
-    const trabajadores = await Usuario.find({trabajador:true, ciudad:usuario.ciudad})
+    const trabajadores = await Usuario.find({ trabajador: true, ciudad: usuario.ciudad })
     const cercanos = trabajadores.filter(trabajador => trabajador._id != usuario._id)
-    return resp.json({cercanos})
+    return resp.json({ cercanos })
 })
 
-router.post('/buscador', verificarToken, async (req, resp) => {
-    const {empleo, ciudad} = req.body
+router.post('/buscador', verificarToken, async(req, resp) => {
+    const { empleo, ciudad } = req.body
     const usuario = req.usuario
     let todos = []
-    if(empleo != '' && ciudad != '') todos = await Usuario.find({trabajador:true, empleo, ciudad}).collation({locale:'es', strength:2})
-    else if(empleo != '') todos = await Usuario.find({trabajador:true, empleo}).collation({locale:'es', strength:2})
-    else if(ciudad != '') todos = await Usuario.find({trabajador:true, ciudad}).collation({locale:'es', strength:2})
-    else todos = await Usuario.find({trabajador:true})
+    if (empleo != '' && ciudad != '') todos = await Usuario.find({ trabajador: true, empleo, ciudad }).collation({ locale: 'es', strength: 2 })
+    else if (empleo != '') todos = await Usuario.find({ trabajador: true, empleo }).collation({ locale: 'es', strength: 2 })
+    else if (ciudad != '') todos = await Usuario.find({ trabajador: true, ciudad }).collation({ locale: 'es', strength: 2 })
+    else todos = await Usuario.find({ trabajador: true })
     const trabajadores = todos.filter(trabajador => trabajador._id != usuario._id)
-    return resp.json({trabajadores})
+    return resp.json({ trabajadores })
 })
 
-router.post('/trabajadores/top',verificarToken , async (req,resp) => {
+router.post('/trabajadores/top', verificarToken, async(req, resp) => {
     const { tipo } = req.body
     let col = null
-    if(tipo == 'local'){
+    if (tipo == 'local') {
         const usuario = req.usuario
-        col = await Usuario.find({trabajador:true, ciudad:usuario.ciudad})
-    }else{
-        col = await Usuario.find({trabajador:true})
+        col = await Usuario.find({ trabajador: true, ciudad: usuario.ciudad })
+    } else {
+        col = await Usuario.find({ trabajador: true })
     }
     const trabajadores = await ordenar(col)
-    return resp.json({trabajadores})
+    return resp.json({ trabajadores })
 
 })
 
@@ -164,15 +165,15 @@ ordenar = async(col) => {
         for (let i = 1; i < col.length; i += 1) {
             let estrellasA = 0
             let estrellasB = 0
-            const trabajosA = await Trabajo.find({autor:col[i - 1]})
-            const trabajosB = await Trabajo.find({autor:col[i]})
+            const trabajosA = await Trabajo.find({ autor: col[i - 1] })
+            const trabajosB = await Trabajo.find({ autor: col[i] })
             for (let index = 0; index < trabajosA.length; index++) {
                 const trabajo = trabajosA[index];
-                estrellasA+=trabajo.estrellas
+                estrellasA += trabajo.estrellas
             }
             for (let index = 0; index < trabajosB.length; index++) {
                 const trabajo = trabajosB[index];
-                estrellasB+=trabajo.estrellas
+                estrellasB += trabajo.estrellas
             }
             if (estrellasA < estrellasB) {
                 listo = false;
--- src/tools/usuario-fotos.js
+++ src/tools/usuario-fotos.js
@@ -1,61 +1,61 @@
 const path = require('path')
 const fs = require('fs')
+const uniqid = require('uniqid')
 
-class UsuarioFotos{
+class UsuarioFotos {
 
-    constructor(){}
+    constructor() {}
 
-    asignarFoto(imagen, usuarioId){
-        return new Promise((resolve,reject) => {
+    asignarFoto(imagen, usuarioId) {
+        return new Promise((resolve, reject) => {
             const pathFoto = this.getCarpetaFoto(usuarioId)
             const foto = this.getFoto(usuarioId)
-            if(foto.length > 0) this.eliminaFoto(pathFoto, foto)
-            const nombreFoto = this.generarNombreUnico(usuarioId,imagen.mimetype)
+            if (foto.length > 0) this.eliminaFoto(pathFoto, foto)
+            const nombreFoto = this.generarNombreUnico(usuarioId, imagen.mimetype)
             imagen.mv(`${pathFoto}/${nombreFoto}`, (err) => {
-                if(err) reject(err)
+                if (err) reject(err)
                 else resolve()
             })
         })
     }
 
-    eliminaFoto(path, foto){
+    eliminaFoto(path, foto) {
         const pathCompleto = path + `/${foto}`
         try {
             fs.unlinkSync(pathCompleto)
-        }catch(err) {
+        } catch (err) {
             console.error(err)
         }
     }
 
-    getFoto(usuarioId){
+    getFoto(usuarioId) {
         const pathFoto = this.getCarpetaFoto(usuarioId)
         const col = fs.readdirSync(pathFoto)
         return col[0] || []
     }
 
-    getCarpetaFoto(usuarioId){
+    getCarpetaFoto(usuarioId) {
         const pathUsuario = this.getCarpetaUsuario(usuarioId)
         const pathFoto = pathUsuario + '/foto'
         const existeFoto = fs.existsSync(pathFoto)
-        if(!existeFoto){
+        if (!existeFoto) {
             fs.mkdirSync(pathFoto)
         }
         return pathFoto
     }
 
-    getCarpetaUsuario(usuarioId){
-        const pathUsuario = path.resolve(__dirname,'../uploads/', usuarioId)
+    getCarpetaUsuario(usuarioId) {
+        const pathUsuario = path.resolve(__dirname, '../uploads/', usuarioId)
         const existe = fs.existsSync(pathUsuario)
-        if(!existe){
+        if (!existe) {
             fs.mkdirSync(pathUsuario)
         }
         return pathUsuario
     }
 
-    generarNombreUnico(usuarioId,tipo){
-        console.log(tipo)
+    generarNombreUnico(usuarioId, tipo) {
         const arr = tipo.split('/')
-        const ext = arr[arr.length-1]
+        const ext = arr[arr.length - 1]
         return `${usuarioId}.${ext}`
     }
 
